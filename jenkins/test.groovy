7ba721a41048bce3c5c38fddc798cf798e8e32cd
tsudhir <theju.sudhir@manheim.com>
2017-03-02 15:36:52 -0500

    updated groovy file

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -1,4 +1,16 @@
+def gitUrl = 'https://github.com/stheju/restapi.git'
+
 job('test job') {
+scm {
+    git {
+      remote {
+        name('origin')
+        url(gitUrl)
+    }
+      branches('master')
+      extensions {}
+    }
+    }
     steps {
         shell('echo Hello World!')
     }
